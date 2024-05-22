a38a83283bc486e0e3e3ac60db049f3185fec50c
Luana Verçosa <vercosa.luana@gmail.com>
2024-05-22 18:46:43 +0000

    Criação de uma view intermediária na camada Silver e adição de joins na dim_customer para inclusão dos dados de endereço dos clientes

--- models/gold/dim_customer.sql
+++ models/gold/dim_customer.sql
@@ -8,12 +8,25 @@ person AS (
     FROM {{ ref('stg_person') }}
 ),
 
+address AS (
+    SELECT *
+    FROM {{ ref('stg_customer_address') }}
+),
+
 joined AS (
     SELECT
     customer.pk_customer,
-    person.person_first_name || ' ' || person.person_middle_name || ' ' || person.person_last_name AS customer
+    person.person_first_name || ' ' || person.person_middle_name || ' ' || person.person_last_name AS customer,
+    address.address,
+    address.city,
+    address.state_province,
+    address.state_province_acronym,
+    address.country,
+    address.country_acronym,
+    address.postal_code
     FROM customer
     LEFT JOIN person ON customer.fk_person = person.pk_business_entity
+    LEFT JOIN address ON customer.pk_customer = address.pk_customer
 )
 
 SELECT *
---
+++ models/silver/intermediate/stg_customer_address.sql
@@ -0,0 +1,44 @@
+WITH customer AS (
+    SELECT *
+    FROM {{ ref('stg_customer') }}
+),
+
+business_entity_address AS (
+    SELECT *
+    FROM {{ ref('stg_business_entity_address') }}
+),
+
+address AS (
+    SELECT *
+    FROM {{ ref('stg_address') }}
+),
+
+state AS (
+    SELECT *
+    FROM {{ ref('stg_state_province') }}
+),
+
+country AS (
+    SELECT *
+    FROM {{ ref('stg_country_region') }}
+),
+
+joined AS (
+    SELECT
+        customer.pk_customer,
+        address.address,
+        address.city,
+        state.nm_state_province AS state_province,
+        state.state_province_code AS state_province_acronym,
+        country.nm_country AS country,
+        country.pk_country AS country_acronym,
+        address.postal_code
+    FROM customer
+    LEFT JOIN business_entity_address ON customer.fk_person = business_entity_address.fk_business_entity
+    LEFT JOIN address ON business_entity_address.fk_address = address.pk_address
+    LEFT JOIN state ON address.fk_state_province = state.pk_state_province
+    LEFT JOIN country ON state.COUNTRY_REGION_CODE = country.pk_country
+)
+
+SELECT *
+FROM joined

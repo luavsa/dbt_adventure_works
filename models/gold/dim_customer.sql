06f9a1227f8363625fd0287a152000e8081091dd
Luana Verçosa <vercosa.luana@gmail.com>
2024-05-28 14:25:48 +0000

    Adição das informações de loja (reseller) na tabela dimensão de clientes

--- models/gold/dim_customer.sql
+++ models/gold/dim_customer.sql
@@ -13,20 +13,26 @@ address AS (
     FROM {{ ref('stg_customer_address') }}
 ),
 
+store AS (
+    SELECT *
+    FROM {{ ref('stg_store') }}
+),
+
 joined AS (
     SELECT
     customer.pk_customer,
     person.person_first_name || ' ' || person.person_middle_name || ' ' || person.person_last_name AS customer,
+    store.pk_business_entity AS pk_store,
+    store.nm_store AS store,
     address.address,
     address.city,
     address.state_province,
-    address.state_province_acronym,
     address.country,
-    address.country_acronym,
     address.postal_code
     FROM customer
     LEFT JOIN person ON customer.fk_person = person.pk_business_entity
     LEFT JOIN address ON customer.pk_customer = address.pk_customer
+    LEFT JOIN store ON customer.fk_store = store.pk_business_entity
 )
 
 SELECT *

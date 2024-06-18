283e2b4dac0410998436b1e62be5e699db8089a5
Luana Verçosa <vercosa.luana@gmail.com>
2024-06-18 20:27:08 +0000

    Alteração na view person e na tabela dimensão clientes para corrigir dados nulos da camada gold

--- models/silver/adventure_works/stg_person.sql
+++ models/silver/adventure_works/stg_person.sql
@@ -1,14 +1,11 @@
 WITH
     person_source AS (
         SELECT
-            CAST(businessentityid AS int) AS pk_business_entity,
+            CAST(businessentityid AS int) AS pk_person,
             CAST(persontype AS varchar) AS person_type,
             CAST(namestyle AS boolean) AS name_style,
             CAST(title AS varchar) AS person_title,
-            CAST(firstname AS varchar) AS person_first_name,
-            CAST(middlename AS varchar) AS person_middle_name,
-            CAST(lastname AS varchar) AS person_last_name,
-            CAST(suffix AS varchar) AS person_suffix,
+            CAST(COALESCE(firstname, '') AS varchar) || ' ' || CAST(COALESCE(middlename, '') AS varchar) || ' ' || CAST(COALESCE(lastname, '') AS varchar) AS person_name,
             CAST(emailpromotion AS string) AS email_promotion,
             CAST(additionalcontactinfo AS string) AS person_additional_contact_info,
             CAST(demographics AS string) AS person_demographics,
